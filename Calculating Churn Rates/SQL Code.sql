e07491dae81d28abea1dc21d9f93e2d8f519c22e
Domskii <90664524+Domskii@users.noreply.github.com>
2022-11-12 23:32:45 +1100

    Update SQL Code.sql

--- Calculating Churn Rates/SQL Code.sql
+++ Calculating Churn Rates/SQL Code.sql
@@ -24,44 +24,36 @@ cross_join AS
   
 --Creating a status temporary table to see how many are active in each month
 status AS
-  (SELECT id, first_day AS month,
+  (SELECT id, first_day AS month,segment,
 CASE
-  WHEN (subscription_start <      first_day) AND (subscription_end > first_day) OR (subscription_end IS NULL)AND (segment = 87) THEN 1
+  WHEN (subscription_start < first_day) AND (subscription_end > first_day) OR (subscription_end IS NULL) THEN 1
   ELSE 0
-  END AS is_active_87,
-CASE 
-  WHEN (subscription_start < first_day) AND (subscription_end > first_day) OR (subscription_end IS NULL) AND (segment = 30) THEN 1
-    ELSE 0
-  END AS is_active_30,
+END AS is_active,
 CASE
-  WHEN (subscription_end BETWEEN first_day AND last_day) AND (segment = 87) THEN 1 
+  WHEN (subscription_end BETWEEN first_day AND last_day) THEN 1 
   ELSE 0
-  END AS is_cancelled_87,
-CASE
-  WHEN (subscription_end BETWEEN first_day AND last_day) AND (segment = 30) THEN 1 
-  ELSE 0
-  END AS is_cancelled_30
+END AS is_cancelled
 FROM cross_join),
 
---Calculating total active users and total user cancelled for each month
+--Temp table to calculate total active users and total user cancelled for each month
 status_aggregate AS
-(SELECT month, SUM(is_active_87) as sum_active_87,
- sum(is_active_30) as sum_active_30,
- sum(is_cancelled_87) as sum_cancelled_87,
- sum(is_cancelled_30) as sum_cancelled_30
+(SELECT month,segment, SUM(is_active) as sum_active,sum(is_cancelled) as sum_cancelled
 FROM status
-GROUP by month)
+GROUP by month, segment)
 
 --Statement to calculate churn rate
-SELECT month, 1.0 * sum_cancelled_87/sum_active_87 as Churn_rate_87,
-1.0 * sum_cancelled_30/sum_active_30 as Churn_rate_30
+SELECT month, segment, 1.0 * sum_cancelled/sum_active as churn_rate
 FROM status_aggregate
+ORDER by Segment
 
 /* Output
-month	      Churn_rate_87	      Churn_rate_30
-2017-01-01	0.0793650793650794	0.0181219110378913
-2017-02-01	0.149494949494949	  0.0287443267776097
-2017-03-01	0.302107728337237	  0.0708263069139966
+month	    segment	  churn_rate
+2017-01-01	30	    0.0232804232804233
+2017-02-01	30	    0.039460020768432
+2017-03-01	30	    0.0895522388059701
+2017-01-01	87	    0.0882723833543506
+2017-02-01	87	    0.167610419026048
+2017-03-01	87	    0.33419689119171
 
 Based on the output, we can tell Churn_rate_30 is out-performing Segment 87.
 This could imply products in segment 30 is better, or their marketing is working better.
